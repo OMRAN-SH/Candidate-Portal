3b90395c1fb7d128e11fa8bb6f016e71ba436ebd
OMRAN-SH <omran.shty@gmail.com>
2024-12-15 07:25:37 +0300

    feat: integrating serilog

--- CandidatePortal/Program.cs
+++ CandidatePortal/Program.cs
@@ -1,7 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using CandidatePortal.Data;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console() 
+    .WriteTo.File("logs/app-.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+
+
+builder.Host.UseSerilog();
+
 
 builder.WebHost.UseWebRoot("wwwroot");
 
@@ -19,6 +31,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IWebHostEnvironment>(builder.Environment);
 
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Replace with your React app's origin
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Configure database context
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -34,5 +58,12 @@ app.UseSwaggerUI();
 
 app.UseAuthorization();
 app.MapControllers();
+
+// Use CORS
+app.UseCors("AllowReactApp");
+
+// Log HTTP requests
+app.UseSerilogRequestLogging();
+
 
 app.Run();
